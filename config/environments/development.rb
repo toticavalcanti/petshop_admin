8edb1d4a1a59c9c870999cbddc430362b184811c
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-18 20:57:33 -0300

    config.action_mailer in config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 1025 }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
