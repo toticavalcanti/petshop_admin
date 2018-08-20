f6856548ea4c4777987d44f568053764515cf201
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-20 20:10:15 -0300

    update app/controllers/admin/sells_controller.rb

--- app/controllers/admin/sells_controller.rb
+++ app/controllers/admin/sells_controller.rb
@@ -1,5 +1,9 @@
 module Admin
-  class SellsController < Fae::BaseController
+  class SellsController &lt; Fae::BaseController
+    before_action :adjust_enum, only: [:create, :update]
 
+    def adjust_enum
+      params[:sell][:status] = params[:sell][:status].to_i
+    end
   end
 end
