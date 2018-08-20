4f03e94b1c1633c23530d0beffff9922efed290e
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-20 19:56:54 -0300

    update /app/models/sell.rb

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,15 +1,15 @@
-class Sell &lt; ApplicationRecord
+class Sell < ApplicationRecord
   include Fae::BaseModelConcern
   enum status: { finished: 0, canceled: 1 }
- 
+
   def fae_display_field
     id
   end
- 
+
   def self.for_fae_index
     order(:id)
   end
- 
+
   belongs_to :discount
   belongs_to :client
 end
