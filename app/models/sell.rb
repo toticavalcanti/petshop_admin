4a9561a5200f943383118aa3ceee66950d6d5f42
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-20 23:07:58 -0300

    insert field validation in app/models/sell.rb

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -2,6 +2,8 @@ class Sell < ApplicationRecord
   include Fae::BaseModelConcern
   enum status: { finished: 0, canceled: 1 }
 
+  validates :client, presence: true
+
   def fae_display_field
     id
   end
