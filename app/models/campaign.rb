4fe4c0f5a717a39c90622d70d44d71d71a328e65
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-21 17:23:02 -0300

    update app/models/campaign.rb

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,14 +1,21 @@
-class Campaign < ApplicationRecord
+class Campaign &lt; ApplicationRecord
   include Fae::BaseModelConcern
-  
+ 
   validates :title, presence: true
   validates :body, presence: true
- 	
   has_many :campaign_clients
   has_many :clients, through: :campaign_clients
-  
+ 
+  after_save :schedule_emails
+ 
   def fae_display_field
     title
   end
-
+ 
+  def schedule_emails
+    Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaingJob.perform_later client, self.title, self.body
+    end
+  end
 end
