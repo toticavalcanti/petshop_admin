3f07d9d4354d6a241fc6b260ba65a9e76364e865
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-20 21:23:42 -0300

    insert field validation in app/models/client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,15 @@
 class Client < ApplicationRecord
   include Fae::BaseModelConcern
+
+  validates :phone, presence: true
+  validates :email,
+  uniqueness: true,
+  presence: true,
+  format: {
+    with: Fae.validation_helpers.email_regex,
+    message: 'You need use a valid and unique email'
+  }
+  
   def fae_display_field
     name
   end
