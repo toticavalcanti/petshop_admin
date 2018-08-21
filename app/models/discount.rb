fafe6326930e05bf365e8f3c6b44eaa1ae6b9b09
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-20 23:01:09 -0300

    insert field validation in app/models/discount.rb

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,7 +2,7 @@ class Discount < ApplicationRecord
   include Fae::BaseModelConcern
 
   validates :title, uniqueness: true, presence: true
-  validates :value, uniqueness: true, presence: true
+  validates :value, presence: true
 
   def fae_display_field
     title
