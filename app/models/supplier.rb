523eaa27f76241e4dd029919642524b054726d9a
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-20 23:03:00 -0300

    insert field validation in app/models/supplier.rb

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,5 +1,16 @@
 class Supplier < ApplicationRecord
   include Fae::BaseModelConcern
+
+  validates :name, uniqueness: true, presence: true
+  validates :phone, presence: true
+  validates :email,
+    uniqueness: true,
+    presence: true,
+    format: {
+      with: Fae.validation_helpers.email_regex,
+      message: 'You need use a valid and unique email'
+    }
+
   def fae_display_field
     name
   end
