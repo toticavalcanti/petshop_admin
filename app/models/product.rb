a379b5ff40077508ccbd1955bfafa91fc22a9a94
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-20 23:07:03 -0300

    insert field validation in app/models/product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
   include Fae::BaseModelConcern
+
+  validates :title, uniqueness: true, presence: true
+  validates :price, presence: true
+
   has_fae_image :hero_image
 
   def fae_display_field
