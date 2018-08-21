34c74cb7699c7eb5a1daf76ce39f0dcf6117a324
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-21 16:33:34 -0300

    update app/models/client.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,15 +1,18 @@
 class Client < ApplicationRecord
   include Fae::BaseModelConcern
-
+ 
+  has_many :campaign_clients
+  has_many :campaigns, through: :campaign_clients, dependent: :destroy
+ 
   validates :phone, presence: true
   validates :email,
-  uniqueness: true,
-  presence: true,
-  format: {
-    with: Fae.validation_helpers.email_regex,
-    message: 'You need use a valid and unique email'
-  }
-  
+    uniqueness: true,
+    presence: true,
+    format: {
+      with: Fae.validation_helpers.email_regex,
+      message: 'You need use a valid and unique email'
+    }
+ 
   def fae_display_field
     name
   end
