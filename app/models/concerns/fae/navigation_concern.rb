57b63efe65e72d6b6a75d50767b7e0d84ab27c4f
toticavalcanti <toticavalcanti@hotmail.com>
2018-08-21 16:15:05 -0300

    menu restructuring in method structure of app/models/concerns/fae/navigation_concern.rb file

--- app/models/concerns/fae/navigation_concern.rb
+++ app/models/concerns/fae/navigation_concern.rb
@@ -25,16 +25,19 @@ module Fae
 
     def structure
       [
-        item('Clients', path: admin_clients_path),
-        item('Products', path: admin_products_path),
-        item('Services', path: admin_services_path),
-        item('Discounts', path: admin_discounts_path),
-        item('Campaigns', path: admin_campaigns_path),
-        item('Suppliers', path: admin_suppliers_path),
-        item('Sells', path: admin_sells_path),
-        # scaffold inject marker
+        item('Manage', subitems: [
+          item('Clients', path: admin_clients_path),
+          item('Discounts', path: admin_discounts_path),
+          item('Suppliers', path: admin_suppliers_path),
+          item('Services', path: admin_services_path),
+          item('Products', path: admin_products_path),
+          item('Sells', path: admin_sells_path),
+        ]),
+        item('Marketing', subitems: [
+          item('Campaigns', path: admin_campaigns_path)
+        ]),
+        item('+ Sell', path: '/admin/sells/new')
       ]
     end
-
   end
 end
